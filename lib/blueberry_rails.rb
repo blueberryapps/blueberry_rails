73430bd6b972f7738da99378414a7264a7e3bb80
Jiří Zajpt <jzajpt@blueberryapps.com>
2014-11-03 16:11:24 +0100

    Require from main project file

--- lib/blueberry_rails.rb
+++ lib/blueberry_rails.rb
@@ -1,4 +1,7 @@
-require "blueberry_rails/version"
+require 'blueberry_rails/version'
+require 'blueberry_rails/generators/app_generator'
+require 'blueberry_rails/action_helpers'
+require 'blueberry_rails/app_builder'
 
 module BlueberryRails
 end
