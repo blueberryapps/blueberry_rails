e4963249ae97fdb44973e2bec280c67c4097756d
Martin Magnusek <magnusekm@gmail.com>
2015-03-16 14:09:50 +0100

    Clean specs

--- spec/blueberry_rails_spec.rb
+++ spec/blueberry_rails_spec.rb
@@ -1,65 +1,33 @@
 require 'minitest/autorun'
-require 'fileutils'
+require './spec/support/blueberry_rails_helpers.rb'
 
-class BlueberryRailsTest < Minitest::Unit::TestCase
+class BlueberryRailsTest < Minitest::Test
+  include BlueberryRailsHelpers
 
   def setup
-    FileUtils.rm_rf 'test_project'
+    remove_project_directory
     cmd 'dropdb test_project_development'
     cmd 'dropdb test_project_test'
-
-    %w[RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |key|
-      set_env key, nil
-    end
   end
 
-  def teardown
-    restore_env
+  def run
+    Bundler.with_clean_env do
+      super
+    end
   end
 
   def test_rake_runs_cleanly
-    create_project 'test_project'
-    assert run_rake('test_project')
+    create_project
+
+    assert_exist_file 'config', 'initializers', 'simple_form.rb'
+    assert run_rake
   end
 
-  def test_rake_runs_cleanly_with_bootstrap_option
-    create_project 'test_project', '--bootstrap'
-    assert run_rake('test_project')
+  def test_rake_runs_with_bootstrap_option
+    create_project '--bootstrap'
+
+    assert_exist_file 'config', 'initializers', 'simple_form.rb'
+    assert_exist_file 'config', 'initializers', 'simple_form_bootstrap.rb'
+    assert run_rake
   end
-
-  protected
-
-  def create_project(project_name, options = nil)
-    bin = File.expand_path(File.join('..', 'bin', 'blueberry_rails'), File.dirname(__FILE__))
-    cmd "#{bin} #{project_name} #{options}"
-  end
-
-  def run_rake(project_name)
-    Dir.chdir(project_name) do
-      cmd 'bundle exec rake db:create'
-      cmd 'bundle exec rake db:migrate'
-      cmd 'bundle exec rake db:test:prepare'
-      cmd 'bundle exec rake'
-    end
-  end
-
-  def cmd(command)
-    system "#{command}"
-  end
-
-  def original_env
-    @original_env ||= {}
-  end
-
-  def set_env(key, value)
-    original_env[key] = ENV.delete(key)
-    ENV[key] = value
-  end
-
-  def restore_env
-    original_env.each do |key, val|
-      ENV[key] = val
-    end
-  end
-
 end
---
+++ spec/support/blueberry_rails_helpers.rb
@@ -0,0 +1,42 @@
+require 'fileutils'
+
+module BlueberryRailsHelpers
+  APP_NAME = 'test_project'
+
+  def remove_project_directory
+    FileUtils.rm_rf(project_path)
+  end
+
+  def create_project(options = nil)
+    cmd "#{blueberry_rails_bin} #{APP_NAME} #{options}"
+  end
+
+  def run_rake
+    Dir.chdir(project_path) do
+      cmd 'bundle exec rake db:create'
+      cmd 'bundle exec rake db:migrate'
+      cmd 'bundle exec rake db:test:prepare'
+      cmd 'bundle exec rake'
+    end
+  end
+
+  def cmd(command)
+    system "#{command}"
+  end
+
+  def project_path
+    @project_path ||= Pathname.new("#{root_path}/#{APP_NAME}")
+  end
+
+  def blueberry_rails_bin
+    File.join(root_path, 'bin', 'blueberry_rails')
+  end
+
+  def root_path
+    File.expand_path('../../../', __FILE__)
+  end
+
+  def assert_exist_file(*path)
+    assert File.exists?(File.join(project_path, *path))
+  end
+end
